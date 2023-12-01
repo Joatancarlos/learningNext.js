0c66b75917424f077404ff4a2b05381c0294ea9e
Joatan Carlos Farias Feitosa <joatancarlos@gmail.com>
2023-12-01 11:39:03 -0300

    ajeitando o erro da fonte

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Roboto } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] })
+export const roboto = Roboto({ subsets:['latin'], weight: ["300", "400", "700"] })
+// export const lusitana = Lusitana({ weight: ["400", "700"], subsets:['latin'] })
